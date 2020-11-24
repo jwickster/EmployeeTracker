c13cbd9bf9136af09ee212570ea8edb2bf4c1e5d
Josh Wickster <joshwickster@gmail.com>
2020-11-24 15:51:28 -0700

    test commit 11/24

--- connection.js
+++ connection.js
@@ -1,5 +1,7 @@
+//Need those dependancies
 var mysql = require("mysql");
 
+//create connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3300,
@@ -7,12 +9,13 @@ var connection = mysql.createConnection({
   database: "employee_tracker"
 
 });
-
+//instantiate the connection
 connection.connect(function (err) {
   if (err)
     throw err;
+  //console log error
   console.log(err);
-  //else
+  //else show thread 
   console.log("connected as id " + connection.threadId);
 });
 
