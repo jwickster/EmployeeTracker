f3617ba6cb04ba69088f4e7ef28f37d1d13a50af
Josh Wickster <joshwickster@gmail.com>
2020-12-05 17:45:10 -0700

    edits

--- Employee-Schema.sql
+++ Employee-Schema.sql
@@ -1,5 +1,3 @@
-# Assistance with from https://www.npmjs.com/package/mysql#install
-
 
 DROP DATABASE IF EXISTS Employee_Tracker;
 
@@ -19,14 +17,12 @@ CREATE TABLE department(
 CREATE TABLE role(
     -- https://dev.mysql.com/doc/mysql-tutorial-excerpt/5.7/en/example-auto-increment.html
     id integer auto_increment not null,
-
     title VARCHAR (30) not null,
-
     salary DECIMAL not null,
-
     department_id INTEGER not null,
-
-    CONSTRAINT fk_department_id foreign key (department_id) references department(id),
+    CONSTRAINT fk_department_id
+        foreign key (department_id)
+            references department(id),
 
     primary key(id)
 );
@@ -43,7 +39,7 @@ CREATE TABLE employee(
     constraint fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
     -- **manager_id** - INT to hold reference to another employee that manager of the current employee.
     -- This field may be null if the employee has no manager
-    manager_id integer,
+    manager_id integer not null,
     constraint fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id),
     
     primary key(id)
@@ -53,25 +49,41 @@ select * from employee;
 select * from role;
 select * from department;
 
-INSERT into department (name)
-VALUES ("Sales");
 
-INSERT into department (name)
-VALUES ("Engineering");
+# INSERT into department
+# VALUES    (44,"Tony"),
+#           (34, "Alfred"),
+#           (54,"Beckett");
+#
+#
+# Insert into role
+# VALUES (155, "Mr",100.00, 50),
+#        (155, "Boss",100.00, 23),
+#        (255, "Pleb",5.00, 5),
+#        (1, "POTUS", 400000,3);
+#
+# Insert into employee
+# VALUES (5, "Thanos","Nana",8675309, 25),
+#        (34, "Epods","Jacob",5656, 15),
+#        (15, "Junke","Bobba",45, 34);
 
-INSERT into department (name)
-VALUES ("Finance");
 
-INSERT into department (name)
-VALUES ("Legal");
 
-INSERT into department (name)
-VALUES ("Manager");
+
+
+INSERT into department
+VALUES (14, "Defense");
+
+INSERT into department
+VALUES (24, "Music");
+
+INSERT into department
+VALUES (34, "Books");
 
 select * from department;
 
-INSERT into role (title, salary, department_id)
-VALUES ("Sales Lead", 45000, 1);
+INSERT into role
+VALUES (5, "The Boss", 56000, 5);
 
 INSERT into role (title, salary, department_id)
 VALUES ("Salesperson", 35000, 1);
@@ -85,8 +97,11 @@ VALUES ("Accountant", 50000, 3);
 INSERT into role (title, salary, department_id)
 VALUES ("Legal", 50000, 4);
 
-INSERT into role (title, salary, department_id)
-VALUES ("Manager", 65000, 5);
+INSERT into role (title, salary, department_id, manager_id)
+VALUES ("Manager", 65000, 5, 56);
+
+INSERT into role
+VALUES ("Manager", 65000, 5, 56);
 
 select * from role;
 
@@ -105,7 +120,7 @@ values ("Beckett", "Wickster", 6);
 INSERT into employee (first_name, last_name, role_id)
 values ("Susan", "Anderson", 7);
 
-INSERT into employee (first_name, last_name, role_id)
+INSERT into employee
 values ("Linda", "Staley", 8);
 
 select * from employee;
--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 var inquirer = require("inquirer");
-var connection = require('./connection.js');
-
-
-
-const viewPosition = [
-  "Departments",
-  "Roles",
-  "Employees",
+var connection = require('./connection');
+const viewOptions = [
+  "View Departments",
+  "View Roles",
+  "View Employees",
   "Update Employee",
-  "Exit"
-];
-
-const selectEmployees = [
-  "Micky Mouse",
-  "Tonya Harding",
-  "Abe Lincoln",
-  "Beckett Wickster",
-  "Taiden Wickster",
-  "Susan Anderson",
-  "Linda Staley",
   "exit"
 ];
 
-const updateName = [
+const employeeOptions = [
+  "George Washington",
+  "Abe Lincoln",
+  "Thomas Jefferson",
+  "Bill Clinton",
+  "Barak Obama",
+  "Joe Biden",
+  "Oprah Winfrey",
+  "exit"
+];
+
+const updateOptions = [
   "First Name",
   "Last Name",
   "Role",
@@ -32,108 +29,83 @@ const updateName = [
 runSearch();
 
 function runSearch() {
-  inquirer.prompt({
-    name: "action",
-    type: "list",
-    message: "What would you like to do?",
-    choices: viewPosition
-  }).then(function (answer) {
-
-    //TODO Switch
-    if (answer.action === viewPosition[0]) {
-      console.log("Departments");
-      departmentView();
-    } else if (answer.action === viewPosition[1]) {
-      console.log("Roles");
-      roleView();
-    } else if (answer.action === viewPosition[2]) {
-      console.log("Employees");
-      employeeView();
-    } else if (answer.action === viewPosition[3]) {
-      console.log("Update Employees");
-      updateEmployee();
-      connection.end();
-    } else if (answer.action === updateName[4]) {
-      console.log("Exit");
-      connection.end();
-    } else {
-      alert("Not working " + answer);
-      console.log(stackTrace);
-    }
-  });
+  inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: viewOptions
+      })
+      .then(function (answer) {
+        switch (answer.action) {
+          case viewOptions[0]:
+            departmentView();
+            break;
+          
+          case viewOptions[1]:
+            roleView();
+            break;
+          
+          case viewOptions[2]:
+            employeeView();
+            break;
+          
+          case viewOptions[3]:
+            updateEmployee();
+          
+          case updateOptions[4]:
+            connection.end();
+            break
+        }
+      })
 }
 
 
 
 function departmentView() {
   var sqlStr = "SELECT * FROM department";
-
   connection.query(sqlStr, function (err, result) {
-    if (err)
-      throw err;
-    //else console log the result and runSearch method
-    console.table(result);
+    if (err) throw err;
+    
+    console.table(result)
     runSearch();
-  });
+  })
 }
 
 function employeeView() {
   var sqlStr = "SELECT first_name, last_name, title, salary FROM employee ";
-
-  for (const leftjoinRoleElement of sqlStr += 'LEFT JOIN role ') {
-    sqlStr += "ON employee.role_id = role.id";
-  };
-  /*
-   sqlStr += "ON employee.role_id = role.id"
-  */
-
+  sqlStr += "LEFT JOIN role ";
+  sqlStr += "ON employee.role_id = role.id"
   connection.query(sqlStr, function (err, result) {
-    if (err)
-      throw err;
-    //Else
-    console.table(result);
+    if (err) throw err;
+    
+    console.table(result)
     runSearch();
-  });
+  })
 }
 
 function roleView() {
   var sqlStr = "SELECT * FROM role";
-
   connection.query(sqlStr, function (err, result) {
     if (err)
       throw err;
-    //Else
     console.table(result)
     runSearch();
   })
 }
 
 
-const updateEmployee = function () {
-
+const updateEmployee = () => {
+  
   function runUpdateSearch() {
     inquirer
-      .prompt({
-        name: 'action',
-        type: 'list',
-        message: 'Which employee do you want to update?',
-        choices: selectEmployees
-      });
-
+        .prompt({
+          name: "action",
+          type: "list",
+          message: "Which employee do you want to update?",
+          choices: employeeOptions
+        })
+    
   }
-
   runUpdateSearch();
-};
-
-
-// const updateWorker = function() {
-//   function run
-//   inquirer
-//       .prompt({
-//         name: "action",
-//         type: "list",
-//         message: "Which worker do you want to update?",
-//         choice: employeeOptions
-//       })
-//   runUpdateSearch();
-// }
+}
